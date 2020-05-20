14ae2fba6a0202093899a2faf940da0f8e19c329
drozdov <konstantin.drozdov@diymaxeda.com>
2020-05-20 17:11:28 +0300

    Some refactoring

--- src/test/java/com/example/demoPactConsumer/PactTest.java
+++ src/test/java/com/example/demoPactConsumer/PactTest.java
@@ -38,7 +38,7 @@ public class PactTest {
                         .integerType("age")
                         .stringValue("postalCode", "1111")
                         .and("firstName","Vitalik")
-                        .and("lastName","Johnson")
+                        .stringType("lastName")
                         .and("position","Admin"))
                 .toPact();
     }
